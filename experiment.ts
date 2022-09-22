932fb1916f7ecbce75e69746d5a8f09d63e9ddae
dimakorzhovnik <korzhovnik.dima@gmail.com>
2022-09-23 01:20:09 +0300

    init

--- experiment.ts
+++ experiment.ts
@@ -2,14 +2,12 @@ import { promises as fsPromises } from "fs"
 import { DirectSecp256k1HdWallet, OfflineDirectSigner } from "@cosmjs/proto-signing"
 import { SigningCyberClient, CyberClient } from "@cybercongress/cyber-js"
 
-const rpc = "http://127.0.0.1:26657" // local testnet
+const rpc = "https://rpc.space-pussy-1.cybernode.ai" // testnet
 
 // testnet
 // CHAIN_ID: 'space-pussy-1',
 // NODE_URL_RPC: 'https://rpc.space-pussy-1.cybernode.ai',
 // NODE_URL_LCD: 'https://lcd.space-pussy-1.cybernode.ai',
-
-const bobAddress: string = "bostrom1apfeqr3mpfnpe8h3tmglzpm4yp6wkkv2g85srj"
 
 const getAliceSignerFromMnemonic = async (): Promise<OfflineDirectSigner> => {
   return DirectSecp256k1HdWallet.fromMnemonic(
@@ -18,6 +16,27 @@ const getAliceSignerFromMnemonic = async (): Promise<OfflineDirectSigner> => {
       prefix: "bostrom",
     }
   )
+}
+
+const checkPersonalBandwidth = async (client: CyberClient, alice: string): Promise<boolean> => {
+  try {
+    const response = await client.price()
+    const priceLink = response.price.dec * 10 ** -18
+
+    const responseAccountBandwidth = await client.accountBandwidth(alice)
+    const { maxValue, remainedValue } = responseAccountBandwidth.neuronBandwidth
+
+    if (maxValue === 0 || remainedValue === 0) {
+      return false
+    } else if (Math.floor(remainedValue / (priceLink * 1000)) === 0) {
+      return false
+    }
+
+    return true
+  } catch (error) {
+    console.log('error', error)
+    return false
+  }
 }
 
 const runAll = async (): Promise<void> => {
@@ -37,6 +56,24 @@ const runAll = async (): Promise<void> => {
     ", height:",
     await signingClient.getHeight()
   )
+
+  if (await checkPersonalBandwidth(client, alice)) {
+    const result = await signingClient.cyberlink(
+      alice,
+      "QmRX8qYgeZoYM3M5zzQaWEpVFdpin6FvVXvp6RPQK3oufV",
+      "QmUX9mt8ftaHcn9Nc6SR4j9MsKkYfkcZqkfPTmMmBgeTe5",
+      {
+        amount: [],
+        gas: "200000",
+      }
+    )
+    console.log('result', result)
+  } else {
+    console.log("not enough personal bandwidth")
+    return;
+  }
+
+
 }
 
 runAll()
